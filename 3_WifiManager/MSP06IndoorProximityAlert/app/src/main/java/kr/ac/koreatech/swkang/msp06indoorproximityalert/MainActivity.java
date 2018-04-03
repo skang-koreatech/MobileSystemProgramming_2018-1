c79f323dd345da7e9b8b8dacad9f92e4539cf9a1
Seungwoo Kang <swkang@koreatech.ac.kr>
2018-04-03 13:54:29 +0900

    WifiManager example

--- 3_WifiManager/MSP06IndoorProximityAlert/app/src/main/java/kr/ac/koreatech/swkang/msp06indoorproximityalert/MainActivity.java
+++ 3_WifiManager/MSP06IndoorProximityAlert/app/src/main/java/kr/ac/koreatech/swkang/msp06indoorproximityalert/MainActivity.java
@@ -1,4 +1,4 @@
-package kr.ac.koreatech.swkang.msp05wifiapscan;
+package kr.ac.koreatech.swkang.msp06indoorproximityalert;
 
 import android.Manifest;
 import android.content.BroadcastReceiver;
@@ -12,9 +12,8 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,14 +21,18 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView scanResultText;
-    WifiManager wifiManager;
-    List<ScanResult> scanResultList;
     boolean isPermitted = false;
     final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
 
-    // BroadcastReceiver 정의
-    // 여기서는 이전 예제에서처럼 별도의 Java class 파일로 만들지 않았는데, 어떻게 하든 상관 없음
+    TextView ap1;
+    TextView ap2;
+    EditText placeName;
+    String top1APId;
+    int top1rssi = -100;
+
+    WifiManager wifiManager;
+    List<ScanResult> scanResultList;
+
     BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -39,15 +42,30 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    // wifi scan 결과를 얻어서 UI의 TextView에 표시하는 기능 수행
+    // RSSI 값이 가장 큰 AP 정보를 얻기 위한 메소드
     private void getWifiInfo() {
         scanResultList = wifiManager.getScanResults();
-        scanResultText.setText("===================================\n");
-        for(int i = 0; i < scanResultList.size(); i++) {
+        String top1BSSID;
+        String top1SSID;
+
+        // 신호 세기가 가장 센 AP의 정보를 저장하기 위한 변수를 scanResultList의 첫번째 결과로 초기화
+        // top1rssi: AP의 RSSI 값, top1BSSID: AP의 BSSID 값, top1SSID: AP의 SSID 값
+        top1rssi = scanResultList.get(0).level;
+        top1BSSID = scanResultList.get(0).BSSID;
+        top1SSID = scanResultList.get(0).SSID;
+
+        // RSSI 크기가 가장 큰 것의 BSSID, SSID, RSSI 값을 얻음
+        for(int i = 1; i < scanResultList.size(); i++) {
             ScanResult result = scanResultList.get(i);
-            scanResultText.append((i+1) + "- SSID: " + result.SSID + "\t RSSI: " + result.level + " dBm\n");
+            if (top1rssi <= result.level) {
+                top1rssi = result.level;
+                top1BSSID = result.BSSID;
+                top1SSID = result.SSID;
+            }
         }
-        scanResultText.append("===================================");
+        // 화면의 TextView에 SSID와 BSSID를 이어붙여서 텍스트로 표시
+        top1APId = top1SSID + top1BSSID;
+        ap1.setText(top1APId);
     }
 
     @Override
@@ -57,11 +75,11 @@ public class MainActivity extends AppCompatActivity {
 
         requestRuntimePermission();
 
-        scanResultText = (TextView)findViewById(R.id.result);
-        scanResultText.setMovementMethod(new ScrollingMovementMethod());
-        wifiManager = (WifiManager)getApplicationContext().getSystemService(WIFI_SERVICE);
+        ap1 = (TextView)findViewById(R.id.ap1);
+        ap2 = (TextView)findViewById(R.id.ap2);
+        placeName = (EditText)findViewById(R.id.placeName);
 
-        // wifi가 활성화되어있는지 확인 후 꺼져 있으면 켠다
+        wifiManager = (WifiManager)getApplicationContext().getSystemService(WIFI_SERVICE);
         if(wifiManager.isWifiEnabled() == false)
             wifiManager.setWifiEnabled(true);
     }
@@ -69,8 +87,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        // wifi scan 결과가 나왔을 때 전송되는 broadcast를 받기 위해
-        // IntentFilter 객체를 생성하고 이를 이용하여 BroadcastReceiver 객체를 등록한다
+
+        // wifi scan 결과 수신을 위한 BroadcastReceiver 등록
         IntentFilter filter = new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
         registerReceiver(mReceiver, filter);
     }
@@ -78,22 +96,56 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+
+        // wifi scan 결과 수신용 BroadcastReceiver 등록 해제
         unregisterReceiver(mReceiver);
     }
 
     public void onClick(View view) {
         if(view.getId() == R.id.start) {
+            // Start wifi scan 버튼이 눌렸을 때
+
             Toast.makeText(this, "WiFi scan start!!", Toast.LENGTH_LONG).show();
 
             if(isPermitted) {
                 // wifi 스캔 시작
                 wifiManager.startScan();
-
-                scanResultText.setText("");
             } else {
                 Toast.makeText(getApplicationContext(),
                         "Location access 권한이 없습니다..", Toast.LENGTH_LONG).show();
             }
+            ap1.setText("");
+        }
+        else if(view.getId() == R.id.startAlert) {
+            // Start proximity alert 버튼이 눌렸을 때
+
+            // placeName이라는 변수로 참조하는 EditText에 쓰여진 장소 이름으로 proximity alert을 등록한다
+
+            // proximity alert을 주는 것은 Service로 구현
+            // Service를 AlertService라는 이름의 클래스로 구현하고 startService 메소드를 호출하여 이 Service를 시작
+
+            if(placeName.getText().toString().equals("")) {
+                Toast.makeText(this, "Input your place name", Toast.LENGTH_LONG).show();
+            } else {
+                String name = placeName.getText().toString();
+
+                Intent intent = new Intent(this, AlertService.class);
+                intent.putExtra("place", name);
+                intent.putExtra("apid", top1APId);
+                intent.putExtra("rssi", top1rssi);
+                // 위에서 key 값으로 쓰인 String 값은 여러 곳에서 반복해서 사용된다면
+                // String 상수로 정의해 놓고 사용하는 것이 좋음
+                // 이 예제에서는 AlertService에서 쓰임
+
+                startService(intent);
+                placeName.setText("");
+            }
+        }
+        else if(view.getId() == R.id.stopAlert) {
+            // Stop proximity alert 버튼이 눌렸을 때
+
+            // AlertService 동작을 중단
+            stopService(new Intent(this, AlertService.class));
         }
     }
 
